b5e7d9620e76aa93825229495bfbdcf2b06441e3
rkiang <ryanlkiang@gmail.com>
2022-12-12 19:43:57 -0600

    created initial mock data, and created initial get request for table

--- server/server.js
+++ server/server.js
@@ -1,9 +1,21 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const db = require("./config/database");
 
 app.use(express.json());
 app.use(cors());
+
+app.get("/user", (req, res) => {
+    db.query("SELECT * FROM user", (err, result) => {
+        if (err) {
+            console.log(err);
+        } else {
+            res.send(result);
+            console.log(result);
+        }
+    });
+});
 
 app.listen(3001, () => {
     console.log("Server running on port 3001");
