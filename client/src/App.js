1fee087018846be91aff0bd292dfbe42f574f4b4
rkiang <ryanlkiang@gmail.com>
2022-12-19 19:38:57 -0600

    imported pages properly

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from "./Pages/Home";
+import Registeration from "./Pages/Registeration";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/"></Route>
+        <Route path="/" element={<Home />}/>
+        <Route path="/register" element={<Registeration />}/>
       </Routes>
     </Router>
   );
