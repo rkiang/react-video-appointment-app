0a33adf4681ccf2a0a3dc23d3fc97e509d5e4c92
rkiang <ryanlkiang@gmail.com>
2022-12-19 19:59:46 -0600

    added profile page with useParams

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Home from "./Pages/Home";
 import Registeration from "./Pages/Registeration";
+import Profile from "./Pages/Profile";
 import ErrorPage from "./Pages/ErrorPage";
 
 function App() {
@@ -10,10 +11,12 @@ function App() {
       <nav>
         <Link to="/"> Home </Link>
         <Link to="/register"> Register </Link>
+        <Link to="/profile"> Profile </Link>
       </nav>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/register" element={<Registeration />} />
+        <Route path="/profile/:username" element={<Profile />} />
         <Route path="*" element={<ErrorPage />} /> {/* path="*" must be last Route to work properly */}
       </Routes>
     </Router>
