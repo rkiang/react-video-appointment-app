276d08cde1b88ddd4992253c18028dc0abeb5f64
rkiang <ryanlkiang@gmail.com>
2022-12-20 10:11:05 -0600

    added initial regex and imported fontawesome

--- client/src/Pages/Registeration.js
+++ client/src/Pages/Registeration.js
@@ -1,6 +1,10 @@
-import React, { useState } from 'react';
+import React, { useref, useState, useEffect } from 'react';
 import { useNavigate } from "react-router-dom";
+import { faCheck, faTimes, faInfoCircle } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
+const UN_REGEX = /^[a-zA-Z][a-zA-Z0-9_]{3,23}$/;
+const PW_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%]).{8-24}$/;
 function Registration() {
     const navigate = useNavigate();
     return (
