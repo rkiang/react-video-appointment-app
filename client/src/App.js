011b2d8d34b7fb0bfd2dd845ed83f00c00d19025
rkiang <ryanlkiang@gmail.com>
2022-12-26 19:28:06 -0600

    added regex and fontawesome for username email form

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import React, { Component }  from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Home from "./Pages/Home";
 import Registeration from "./Pages/Registeration";
