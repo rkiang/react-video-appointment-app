ec509ff6111e4140bb46c63eba220a57a4af4f89
rkiang <ryanlkiang@gmail.com>
2023-01-24 10:05:31 -0600

    fixed sql query and imported db from config folder

--- server/routes/Registration.js
+++ server/routes/Registration.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const router = express.Router();
 const bcrypt = require("bcrypt");
-const mysql = require("mysql");
+// const mysql = require("mysql");
+const db = require("../config/database");
 
 router.post("/", async (req, res) => {
     const username = req.body.username;
@@ -10,7 +11,7 @@ router.post("/", async (req, res) => {
     const fName = req.body.fName;
     const lName = req.body.lName;
 
-    db.query("INSERT INTO user (username, email, fName, lName, password) VALUES (?, ?, ?, ?, ?",
+    db.query("INSERT INTO user (username, email, fName, lName, password) VALUES (?, ?, ?, ?, ?)",
         [username, email, fName, lName, password],
         (err, result) => {
             console.log(err);
