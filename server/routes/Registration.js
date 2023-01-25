588c023a29718e6e12650aae146730e85689551b
rkiang <ryanlkiang@gmail.com>
2023-01-25 10:08:32 -0600

    bcrypt implemented, passwords are hashed when inserted to DB

--- server/routes/Registration.js
+++ server/routes/Registration.js
@@ -1,20 +1,30 @@
 const express = require("express");
 const router = express.Router();
 const bcrypt = require("bcrypt");
+const saltRounds = 10;
 // const mysql = require("mysql");
 const db = require("../config/database");
 
 router.post("/", async (req, res) => {
     const username = req.body.username;
     const password = req.body.password;
+
+    // Using Bcrypt for password input to hash to db query
+    const encryptedPW = await bcrypt.hash(password, saltRounds);
+
     const email = req.body.email;
     const fName = req.body.fName;
     const lName = req.body.lName;
-
     db.query("INSERT INTO user (username, email, fName, lName, password) VALUES (?, ?, ?, ?, ?)",
-        [username, email, fName, lName, password],
+        [username, email, fName, lName, encryptedPW],
         (err, result) => {
-            console.log(err);
+            if (err) {
+                console.log(err);
+            } else {
+                if (result) {
+                    res.send(result);
+                }
+            }
         }
     );
 });
