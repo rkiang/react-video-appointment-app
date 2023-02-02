83a7fe47e7152b78a783e42f00e22317d83ee11d
rkiang <ryanlkiang@gmail.com>
2023-02-01 19:36:10 -0600

    added route to login page on app.js, added basic form for login inputs

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import React, { Component }  from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Home from "./Pages/Home";
+import Login from "./Pages/Login";
 import Registeration from "./Pages/Registeration";
 import Profile from "./Pages/Profile";
 import ErrorPage from "./Pages/ErrorPage";
@@ -11,11 +12,13 @@ function App() {
     <Router>
       <nav>
         <Link to="/"> Home </Link>
+        <Link to="/login"> Login </Link>
         <Link to="/register"> Register </Link>
         <Link to="/profile"> Profile </Link>
       </nav>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Registeration />} />
         <Route path="/profile/:username" element={<Profile />} />
         <Route path="*" element={<ErrorPage />} /> {/* path="*" must be last Route to work properly */}
--- client/src/Pages/Login.js
+++ client/src/Pages/Login.js
@@ -1,8 +1,33 @@
-import React from 'react';
+import React, { useRef, useState, useEffect, Component } from 'react';
 
 function Login() {
+
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
     return (
-        <div>Login</div>
+        <div>
+            <h3>Login</h3>
+            <h5>Enter your email address and password to continue.</h5>
+            <form>
+                <label>
+                    Email Address:
+                    <input type="text" name="email"
+                        onChange={(e) => { setEmail(e.target.value) }} />
+                </label>
+                <label>
+                    Password:
+                    <input type="password" name="password"
+                        onChange={(e) => { setPassword(e.target.value) }} />
+                </label>
+                <br />
+                <input type="submit" value="Submit" />
+            </form>
+            <div>
+                <h4>Don't have an account? <a href="/register">Register here</a></h4>
+
+            </div>
+        </div>
     )
 }
 
